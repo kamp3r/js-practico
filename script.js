fc43f8a39f86b3701adbadb88ffbe4eb2e007e9b
Martin Heredia <martinheredia.dev@gmail.com>
2023-05-23 04:25:42 -0300

    Added aside detail product

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const hamburgerMenu = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const asideCart = document.querySelector('.product-detail')
+const asideCart = document.querySelector('#shoppingCart')
 
 
 function toggleDesktopMenu(){
